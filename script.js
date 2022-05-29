57fe0fa5f02003f82b8284c56b9436fa3d77250f
Jan Odvar Grindheim <jan.odvar@protonmail.com>
2022-05-29 13:37:58 +0200

    new

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const heightInput = document.querySelector(`.height`);
 const weightInput = document.querySelector(`weight`);
 const bmiOutput = document.querySelector(`.bmioutput`);
 
-console.log("test");
-s;
+function calcBMI(heightInput, weightInput) {
+  const bmi = (weightInput / heightInput ** 2) * 10000;
+  return bmi;
+}
+
+/*
+BMI 	Nutritional Status
+Below 18.5 	Underweight
+18.5 – 24.9 	Normal Weight
+25.0 – 29.9 	Pre-obesity
+30.0 – 34.9 	Obesity Class I
+35 – 39.9 	Obesity Class II
+Above 40 	Obesity Class III 
+*/
