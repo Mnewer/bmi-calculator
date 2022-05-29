75b03e1b53bc9033996dc181cde0fdef50bef952
Jan Odvar Grindheim <jan.odvar@protonmail.com>
2022-05-29 12:53:28 +0200

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const heightInput = document.querySelector(`.height`);
+const weightInput = document.querySelector(`weight`);
+const bmiOutput = document.querySelector(`.bmioutput`);
+
+console.log("test");
